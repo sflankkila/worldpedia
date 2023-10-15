0584ce3d363d1283c9925b506d4e1ed31be06c85
sflankkila <simonflankkila@gmail.com>
2023-10-15 21:05:24 +0300

    Versioning untracked files

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+  }
